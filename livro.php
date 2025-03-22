0b4896abaef659ad96365cf3f6e73c49157d3ec7
Andr3eh <andre.eugenio77@hotmail.com>
2025-03-21 21:59:04 -0300

    Primeiro Commit

---
+++ livro.php
@@ -0,0 +1,54 @@
+<?php 
+
+class Livro{
+
+private $titulo
+private $autor
+private $ano_publicacao;
+private $preco;
+
+public function _construct($titulo, $autor, $ano_publicacao, $preco){
+    $this->$titulo = $titulo;
+    $this->$autor = $autor;
+    $this->$ano_publicacao = $ano_publicacao;
+    $this->$preco = $preco;
+}
+
+public function getTitulo(){
+    return $this->titulo;
+}
+
+public function getAutor(){
+    return $this->autor;
+}
+
+public function getAno_publicacao(){
+    return $this->ano_publicacao;
+}
+public function getPreco(){
+    return $this->preco;
+}
+
+public function setPreco($valor){
+    $this->preco = $valor;
+}
+
+$Book = new Livro();
+$Book2 = new Livro();
+
+$Book->getTitulo('Os Miseraveis');
+$Book->getAutor('Victor Hugo');
+$Book->getAno_publicacao('1862');
+$Book->getPreco('R$ 50,00');
+//------------------------------------------------------
+echo "Livro : " .$Book->getTitulo();
+echo "Autor : " .$Book->getAutor();
+echo "Ano de Publicação :" .$Book->getAno_publicacao();
+echo "Preco : " .$Book->getPreco();
+//------------------------------------------------------
+
+}
+
+
+
+?>
